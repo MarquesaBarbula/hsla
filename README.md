6eec5456ed592c71f0edb0cf906901a4dbf0d720
Mariana Barnola <marianabarnola@gmail.com>
2022-02-17 17:58:16 +0100

    commit before upload to Github

---
+++ README.md
@@ -0,0 +1,29 @@
+# HSL color test
+
+Reading The HSB Color System: A Practitioner's Primer by Erik D. Kennedy
+https://learnui.design/blog/the-hsb-color-system-practicioners-primer.html
+inspired me to create this visual guide,
+somewhere to have some hands-on practice on this great and not so much used color system.
+
+
+## Built with
+
+* HTML
+* CSS
+* JavaScript
+
+
+### With the help of
+
+How To Change CSS Values With A Slider! - CSS Custom Properties (Variables) & JavaScript
+https://www.youtube.com/watch?v=FgSfzJPyxhg
+
+HTML5 input type range show range value
+https://stackoverflow.com/questions/10004723/html5-input-type-range-show-range-value
+
+
+# Coded with love by Mariana Barnola
+https://github.com/MarquesaBarbula
+
+;)
+
---
+++ script.js
@@ -0,0 +1,195 @@
+// --try this color slider response
+
+const slider = document.getElementById("new-hue-slider");
+const root = document.documentElement;
+
+slider.addEventListener("input", (e) => {
+    root.style.setProperty("--try_this_color", e.target.value);
+});
+
+
+// custom color slider response
+
+// color 1 - hue1
+
+const sliderhue1 = document.getElementById("hue1");
+const roothue1 = document.documentElement;
+
+sliderhue1.addEventListener("input", (e) => {
+    root.style.setProperty("--try_h_1", e.target.value);
+});
+
+// color 1 - sat1
+
+const slidersat1 = document.getElementById("sat1");
+const rootsat1 = document.documentElement;
+
+slidersat1.addEventListener("input", (e) => {
+    root.style.setProperty("--try_s_1", e.target.value + "%");
+});
+
+// color 1 - lig1
+
+const sliderlig1 = document.getElementById("lig1");
+const rootlig1 = document.documentElement;
+
+sliderlig1.addEventListener("input", (e) => {
+    root.style.setProperty("--try_l_1", e.target.value + "%");
+});
+
+// color 1 - alph1
+
+const slideralph1 = document.getElementById("alph1");
+const rootalph1 = document.documentElement;
+
+slideralph1.addEventListener("input", (e) => {
+    root.style.setProperty("--try_a_1", e.target.value + "%");
+});
+
+
+// color 2 - hue2
+
+const sliderhue2 = document.getElementById("hue2");
+const roothue2 = document.documentElement;
+
+sliderhue2.addEventListener("input", (e) => {
+    root.style.setProperty("--try_h_2", e.target.value);
+});
+
+// color 2 - sat2
+
+const slidersat2 = document.getElementById("sat2");
+const rootsat2 = document.documentElement;
+
+slidersat2.addEventListener("input", (e) => {
+    root.style.setProperty("--try_s_2", e.target.value + "%");
+});
+
+// color 2 - lig2
+
+const sliderlig2 = document.getElementById("lig2");
+const rootlig2 = document.documentElement;
+
+sliderlig2.addEventListener("input", (e) => {
+    root.style.setProperty("--try_l_2", e.target.value + "%");
+});
+
+// color 2 - alph2
+
+const slideralph2 = document.getElementById("alph2");
+const rootalph2 = document.documentElement;
+
+slideralph2.addEventListener("input", (e) => {
+    root.style.setProperty("--try_a_2", e.target.value + "%");
+});
+
+
+// color 3 - hue3
+
+const sliderhue3 = document.getElementById("hue3");
+const roothue3 = document.documentElement;
+
+sliderhue3.addEventListener("input", (e) => {
+    root.style.setProperty("--try_h_3", e.target.value);
+});
+
+// color 3 - sat3
+
+const slidersat3 = document.getElementById("sat3");
+const rootsat3 = document.documentElement;
+
+slidersat3.addEventListener("input", (e) => {
+    root.style.setProperty("--try_s_3", e.target.value + "%");
+});
+
+// color 3 - lig3
+
+const sliderlig3 = document.getElementById("lig3");
+const rootlig3 = document.documentElement;
+
+sliderlig3.addEventListener("input", (e) => {
+    root.style.setProperty("--try_l_3", e.target.value + "%");
+});
+
+// color 3 - alph3
+
+const slideralph3 = document.getElementById("alph3");
+const rootalph3 = document.documentElement;
+
+slideralph3.addEventListener("input", (e) => {
+    root.style.setProperty("--try_a_3", e.target.value + "%");
+});
+
+
+// color 4 - hue4
+
+const sliderhue4 = document.getElementById("hue4");
+const roothue4 = document.documentElement;
+
+sliderhue4.addEventListener("input", (e) => {
+    root.style.setProperty("--try_h_4", e.target.value);
+});
+
+// color 4 - sat4
+
+const slidersat4 = document.getElementById("sat4");
+const rootsat4 = document.documentElement;
+
+slidersat4.addEventListener("input", (e) => {
+    root.style.setProperty("--try_s_4", e.target.value + "%");
+});
+
+// color 4 - lig4
+
+const sliderlig4 = document.getElementById("lig4");
+const rootlig4 = document.documentElement;
+
+sliderlig4.addEventListener("input", (e) => {
+    root.style.setProperty("--try_l_4", e.target.value + "%");
+});
+
+// color 4 - alph4
+
+const slideralph4 = document.getElementById("alph4");
+const rootalph4 = document.documentElement;
+
+slideralph4.addEventListener("input", (e) => {
+    root.style.setProperty("--try_a_4", e.target.value + "%");
+});
+
+
+// color 5 - hue5
+
+const sliderhue5 = document.getElementById("hue5");
+const roothue5 = document.documentElement;
+
+sliderhue5.addEventListener("input", (e) => {
+    root.style.setProperty("--try_h_5", e.target.value);
+});
+
+// color 5 - sat5
+
+const slidersat5 = document.getElementById("sat5");
+const rootsat5 = document.documentElement;
+
+slidersat5.addEventListener("input", (e) => {
+    root.style.setProperty("--try_s_5", e.target.value + "%");
+});
+
+// color 5 - lig5
+
+const sliderlig5 = document.getElementById("lig5");
+const rootlig5 = document.documentElement;
+
+sliderlig5.addEventListener("input", (e) => {
+    root.style.setProperty("--try_l_5", e.target.value + "%");
+});
+
+// color 5 - alph5
+
+const slideralph5 = document.getElementById("alph5");
+const rootalph5 = document.documentElement;
+
+slideralph5.addEventListener("input", (e) => {
+    root.style.setProperty("--try_a_5", e.target.value + "%");
+});
